d6bf3ba2af942634549eb7a67af223d7807f65d8
April-cl <xcl8202@gmail.com>
2023-03-09 16:32:32 +0800

    使用Transition

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -9,11 +9,13 @@ export const routes = [
         path: '/',
         redirect: '/welcome'
     },
-    {path: '/welcome', component: Welcome, children: [
-        {path: '', redirect: '/welcome/1'},
-        {path: '1', component: First},
-        {path: '2', component: Second},
-        {path: '3', component: Third},
-        {path: '4', component: Forth},
-    ]}
+    {
+        path: '/welcome', component: Welcome, children: [
+            { path: '', redirect: '/welcome/1' },
+            { path: '1', component: First },
+            { path: '2', component: Second },
+            { path: '3', component: Third },
+            { path: '4', component: Forth },
+        ]
+    }
 ]
