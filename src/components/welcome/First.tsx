94209cf870efff0bf73befcebfe78c25e960c382
April-cl <xcl8202@gmail.com>
2023-03-15 10:42:01 +0800

    搞定4张欢迎卡片的切换

--- src/components/welcome/First.tsx
+++ src/components/welcome/First.tsx
@@ -1,26 +1,14 @@
-import { defineComponent, ref, watchEffect } from 'vue';
-import { useRouter } from 'vue-router';
-import { useSwipe } from '../../hocks/useSwipe';
+import { defineComponent, FunctionalComponent, ref, watchEffect } from 'vue';
 import s from './Welcome.module.scss';
 
-export const First = defineComponent({
-    setup() {
-        const div = ref<HTMLDivElement>()
-        const router = useRouter()
-        const { swiping, direction } = useSwipe(div, {
-            beforeStart: e => e.preventDefault()
-        })
-        watchEffect(() => {
-            if (swiping.value && direction.value === 'left') {
-                router.push('/welcome/2')
-            }
-        })
-        return () => (
-            <div class={s.card} ref={div}>
-                <svg>
-                    <use xlinkHref='#pig'></use>
-                </svg>
-                <h2>会挣钱<br />还要会省钱</h2>
-            </div>)
-    }
-})
+export const First: FunctionalComponent = () => {
+    return (
+        <div class={s.card}>
+            <svg>
+                <use xlinkHref='#pig'></use>
+            </svg>
+            <h2>会挣钱<br />还要会省钱</h2>
+        </div>)
+}
+
+First.displayName = 'First'
