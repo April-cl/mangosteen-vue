68e7fd5b3d328bcf597627d17adaa8e2e00d489e
April-cl <xcl8202@gmail.com>
2023-03-16 13:58:55 +0800

    创建 FloatButton 组件

--- src/shared/Icon.tsx
+++ src/shared/Icon.tsx
@@ -1,4 +1,5 @@
 import { defineComponent, PropType } from "vue";
+import s from './Icon.module.scss'
 
 export const Icon = defineComponent({
     props: {
@@ -7,10 +8,10 @@ export const Icon = defineComponent({
         }
     },
     setup: (props, context) => {
-        return () => {
-            <svg>
+        return () => (
+            <svg class={s.icon}>
                 <use xlinkHref={'#' + props.name}></use>
             </svg>
-        }
+        )
     }
 })
