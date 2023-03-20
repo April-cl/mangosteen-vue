d96eb3084891effaa727623360a0f37dfaae9b03
April-cl <xcl8202@gmail.com>
2023-03-20 14:02:29 +0800

    引入 vant 实现日期选择

--- src/components/item/InputPad.tsx
+++ src/components/item/InputPad.tsx
@@ -1,5 +1,7 @@
-import { defineComponent, PropType } from "vue";
+import { DatetimePicker, Popup } from "vant";
+import { defineComponent, PropType, ref } from "vue";
 import { Icon } from "../../shared/Icon";
+import { time } from "../../shared/time";
 import s from './InputPad.module.scss'
 
 export const InputPad = defineComponent({
@@ -9,6 +11,9 @@ export const InputPad = defineComponent({
         }
     },
     setup: (props, context) => {
+        const now = new Date()
+        const refDate = ref<Date>(now)
+        const refShowPop = ref(false)
         const buttons = [
             { text: '1', onClick: () => { } },
             { text: '2', onClick: () => { } },
@@ -32,7 +37,14 @@ export const InputPad = defineComponent({
             <div class={s.dateAndAmount}>
                 <span class={s.date}>
                     <Icon name="date" class={s.icon} />
-                    <span>2023-01-01</span>
+                    <span>
+                        <span onClick={() => refShowPop.value = true}>
+                            {time(refDate.value).format()}
+                        </span>
+                        <Popup position='bottom' v-model:show={refShowPop.value}>
+                            <DatetimePicker v-model={refDate.value} type="date" title="选择年月日" onConfirm={() => refShowPop.value = false}></DatetimePicker>
+                        </Popup>
+                    </span>
                 </span>
                 <span class={s.amount}>000.00</span>
             </div>
