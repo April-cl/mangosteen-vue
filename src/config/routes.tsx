a303a3b97ae36dbda7b5366ce8e04f74b7621eca
April-cl <xcl8202@gmail.com>
2023-03-20 15:56:45 +0800

    添加 tag 相关页面

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -1,5 +1,7 @@
 import { ItemCreate } from "../components/item/ItemCreate";
 import { ItemList } from "../components/item/ItemList";
+import { TagCreate } from "../components/tag/TagCreate";
+import { TagEdit } from "../components/tag/TagEdit";
 import { First } from "../components/welcome/First";
 import { FirstActions } from "../components/welcome/FirstActions";
 import { Forth } from "../components/welcome/Forth";
@@ -10,6 +12,7 @@ import { Third } from "../components/welcome/Third";
 import { ThirdActions } from "../components/welcome/ThirdActions";
 import { ItemPage } from "../views/ItemPage";
 import { StartPage } from "../views/StartPage";
+import { TagPage } from "../views/TagPage";
 import { Welcome } from "../views/Welcome";
 
 export const routes = [
@@ -34,6 +37,13 @@ export const routes = [
         children: [
             { path: '', component: ItemList },
             { path: 'create', component: ItemCreate }
+        ]
+    },
+    {
+        path: '/tags', component: TagPage,
+        children: [
+            { path: 'create', component: TagCreate },
+            { path: ':id', component: TagEdit }
         ]
     }
 ]
