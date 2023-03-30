a323319c5df89fae1311f322b0a4059b9710159c
April-cl <xcl8202@gmail.com>
2023-03-30 17:01:37 +0800

    扩展了 Tabs 组件，支持 classPrefix

--- src/components/item/ItemList.tsx
+++ src/components/item/ItemList.tsx
@@ -1,15 +1,35 @@
-import { defineComponent, PropType } from "vue";
+import { defineComponent, PropType, ref } from "vue";
+import { MainLayout } from "../../layouts/MainLayout";
+import { Icon } from "../../shared/Icon";
+import { Tab, Tabs } from "../../shared/Tabs";
 import s from './ItemList.module.scss'
 
 export const ItemList = defineComponent({
     props: {
-        name: {
-            type: String as PropType<string>
-        }
     },
     setup: (props, context) => {
+        const refSelected = ref('本月')
         return () => (
-            <div class={s.wrapper}>hi</div>
+            <MainLayout>{{
+                title: () => '山竹记账',
+                icon: () => <Icon name="menu" />,
+                default: () => (
+                    <Tabs classPrefix={'customTabs'} v-model:selected={refSelected.value}>
+                        <Tab name="本月">
+                            list1
+                        </Tab>
+                        <Tab name="上月">
+                            list2
+                        </Tab>
+                        <Tab name="今年">
+                            list3
+                        </Tab>
+                        <Tab name="自定义时间">
+                            list4
+                        </Tab>
+                    </Tabs>
+                )
+            }}</MainLayout>
         )
     }
 })
